8d18bb5e71646607c5870c228383986dd99f9522
Abdul Waheed Sheik <121844861+Waheed2023@users.noreply.github.com>
2023-01-03 11:00:31 +0530

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text(' 🥣 Oats meal')
 streamlit.text('  🥑 🍞 Apple,banana, garpes, Avacado')
 streamlit.text(' 🐔 hard boiled free range eggs')
 streamlit.text(' 🥗 pure veg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
