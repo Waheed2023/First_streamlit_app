b13a0d66ec43715a0950c8e8c10e9beebef4f364
Abdul Waheed Sheik <121844861+Waheed2023@users.noreply.github.com>
2023-01-03 10:56:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit
 streamlit.title( 'My Parents Healthy Dinner')
 streamlit.header( 'Breakfast Menu')
 streamlit.text(' Oats meal')
-
+streamlit.text(' Apple,banana, garpes')
+streamlit.text(' hard boiled free range eggs')
